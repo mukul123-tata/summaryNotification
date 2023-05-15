ad947fe073b3d17d5f37a0e5a27854e8a19bb51a
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-05-15 11:24:24 +0530

    Initial Commit

---
+++ src/main/java/com/centralizedNotificationEngine/summaryNotifications/SummaryNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.centralizedNotificationEngine.summaryNotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummaryNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummaryNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/centralizedNotificationEngine/summaryNotifications/exceptions/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.centralizedNotificationEngine.summaryNotifications.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message){
+        super(message);
+    }
+}
