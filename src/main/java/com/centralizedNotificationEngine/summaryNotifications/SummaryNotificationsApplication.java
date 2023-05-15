d6b30477172d95babf3844e2552607cfcbd6be92
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2023-05-15 15:11:08 +0530

    Add SQL Crendentials

--- src/main/java/com/centralizedNotificationEngine/summaryNotifications/SummaryNotificationsApplication.java
+++ src/main/java/com/centralizedNotificationEngine/summaryNotifications/SummaryNotificationsApplication.java
@@ -1,13 +1,14 @@
 package com.centralizedNotificationEngine.summaryNotifications;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class SummaryNotificationsApplication {
+public class SummaryNotificationsApplication{
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SummaryNotificationsApplication.class, args);
 	}
-
 }
