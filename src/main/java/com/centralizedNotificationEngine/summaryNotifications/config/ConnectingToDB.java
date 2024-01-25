69a78fe341cbffabfa200ad8582370a3a482ae78
mukul123-tata <130564694+mukul123-tata@users.noreply.github.com>
2024-01-25 14:06:32 +0530

    Updated code

--- src/main/java/com/centralizedNotificationEngine/summaryNotifications/config/ConnectingToDB.java
+++ src/main/java/com/centralizedNotificationEngine/summaryNotifications/config/ConnectingToDB.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class ConnectingToDB {
 
     public void Execute(String sqlStr) throws SQLException {
-     //  Connection developmentConnection = DriverManager.getConnection("jdbc:mysql://10.133.208.200:3306/flt","flt","flt123");
+    //   Connection developmentConnection = DriverManager.getConnection("jdbc:mysql://10.133.208.200:3306/flt","flt","flt123");
      //  Connection uatConnection = DriverManager.getConnection("jdbc:mysql://10.133.232.146:3307/test","suvarnaj","suvarna123");
         Connection productionConnection = DriverManager.getConnection("jdbc:mysql://10.150.10.9:3306/CNS","suvarnaj_prod","Suvja@23");
         Statement statement = productionConnection.createStatement();
